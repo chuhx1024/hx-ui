acf9c03354d1b853c86707011a7075d437284c83
chuhx <hx.chu@clustar.ai>
2020-11-02 17:43:00 +0800

    feat: 封装button的简单实现

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,37 @@
 <template>
   <div id="app">
-      hx
+      <div class="row">
+        <hx-button >按钮</hx-button>
+        <hx-button type="primary">按钮</hx-button>
+        <hx-button type="success">按钮</hx-button>
+        <hx-button type="info">按钮</hx-button>
+        <hx-button type="danger">按钮</hx-button>
+        <hx-button type="warning">按钮</hx-button>
+      </div>
+      <div class="row">
+        <hx-button plain>按钮</hx-button>
+        <hx-button plain type="primary">按钮</hx-button>
+        <hx-button plain type="success">按钮</hx-button>
+        <hx-button plain type="info">按钮</hx-button>
+        <hx-button plain type="danger">按钮</hx-button>
+        <hx-button plain type="warning">按钮</hx-button>
+      </div>
+      <div class="row">
+        <hx-button round plain>按钮</hx-button>
+        <hx-button round plain type="primary">按钮</hx-button>
+        <hx-button round plain type="success">按钮</hx-button>
+        <hx-button round plain type="info">按钮</hx-button>
+        <hx-button round plain type="danger">按钮</hx-button>
+        <hx-button round plain type="warning">按钮</hx-button>
+      </div>
+      <div class="row">
+        <hx-button circle plain>按</hx-button>
+        <hx-button circle plain type="primary">按</hx-button>
+        <hx-button circle plain type="success">按</hx-button>
+        <hx-button circle plain type="info">按</hx-button>
+        <hx-button circle plain type="danger">按</hx-button>
+        <hx-button circle plain type="warning">按</hx-button>
+      </div>
   </div>
 </template>
 
@@ -10,5 +41,11 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+.row {
+    padding: 20px 0;
+    .hx-button {
+        margin-right: 20px ;
+    }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import HxButton from './components/button.vue'
+
+Vue.component(HxButton.name, HxButton)
+
 Vue.config.productionTip = false
 
 new Vue({
